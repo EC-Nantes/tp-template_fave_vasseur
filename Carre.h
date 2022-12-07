c401d47a01609c4d3dcbe8228689131733c2ad7a
François Vasseur <vasseurfrancois11@gmail.com>
2022-12-07 17:28:30 +0100

    Modif affichage carre

--- Carre.h
+++ Carre.h
@@ -26,7 +26,7 @@ Carre<T, S>::Carre(Point<T> p, T cote):Rectangle<T, S>(p, cote, cote){}
 template <typename T, typename S>
 std::ostream& operator<<(std::ostream &output, Carre<T, S> const &c)
 {
-    output << "Rectangle de centre " << c.point_ << "De cote = " << c.largeur_ << std::endl;
+    output << "Carre de centre " << c.point_ << "De cote = " << c.largeur_ << std::endl;
     return output;
 }
 
